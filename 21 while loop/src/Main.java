fa25a36d7d104eff1bb946f44354656c90ddb14b
chanhao2016-maker <chanhao2016@gmail.com>
2025-12-15 20:52:23 +0800

    Class while

--- 21 while loop/src/Main.java
+++ 21 while loop/src/Main.java
@@ -8,19 +8,21 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String name = "";
+        /*String name = "";
 
         while(name.isEmpty()) {
             System.out.print("Enter your name: ");
             name = scanner.nextLine();
         }
 
-        /* while(1 == 1){
+        while(1 == 1){
                System.out.print("I'm stuck");
          }
+
          */
 
-        String response = "";
+
+        /*String response = "";
 
         while(!response.equals("Q")){
             System.out.println("You are playing a game");
@@ -30,9 +32,64 @@ public class Main {
 
         System.out.println("You have Won!");
 
+         */
+
+
+
+
+
+
+
+
+        // while 的话是当条件符合才会进入loop
+        // do- while 的话, 先do 条件, 过后才进loop
+
+
+
+
+
+        /*int age = 0;
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        while(age<0) {
+            System.out.println("If you type negative you will be in the infinite sukiyomi");
+            System.out.println("So enter again your age (wisely): ");
+            age = scanner.nextInt();
+
+
+            if (age < 0) {
+                System.out.println("Mangekyo sharingannnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.println("nnnnnnnnnnnnnnnnnnnnnnn");
+                System.out.print("This is your last chance: ");
+                age = scanner.nextInt();
+
+            }
+
+        }
+
+
+        System.out.println("You are " + age + " years old");
+
+         */
+
+        int number =0;
+
+       do {
+            System.out.print("Enter a number between 1-10:");
+            number = scanner.nextInt();
+        }while(number<1 || number>10);
+
+        System.out.println("You picked " + number);
+
         scanner.close();
-
-
 
     }
         }
