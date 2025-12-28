9d2ebde3b4b1b04773778b53410a33434209b726
chanhao2016-maker <chanhao2016@gmail.com>
2025-12-28 19:04:19 +0800

    learning

---
+++ 34. varangs/src/Main34.java
@@ -0,0 +1,25 @@
+public class Main34{
+    public static void main(String[]args){
+        // varargs = allow a method to accept a varying # of arguments
+        //           makes methods more flexible, no need for overloaded methods
+        //           java will pack the arguments into an array
+        //           ... (ellipsis)
+
+        System.out.println(average());
+
+    }
+    static double average(double... numbers){
+
+        double sum =0;
+
+        if(numbers.length == 0){
+            return 0;
+        }
+
+        for(double number : numbers){
+            sum += number;
+        }
+
+        return sum / numbers.length;
+    }
+}
